bdc2a28adde336da64408763419fc75901c5f7d9
Vlad-Sebastian Cretu <vcretu@bitdefender.com>
2025-04-30 16:06:20 +0300

    fixed method proto print

--- dex2smali/src/dex/proto_id_item.rs
+++ dex2smali/src/dex/proto_id_item.rs
@@ -4,7 +4,7 @@ use crate::{traits::parse::TryParseFromBytes, utils::read_u32_le};
 pub struct ProtoIdItem {
     /// index into the string_ids list for the short-form descriptor string of this prototype. The string must conform to the syntax for ShortyDescriptor, and must correspond to the return type and parameters of this item.
     pub shorty_idx: u32,
-    /// index into the type_ids list for the return type of this prototype
+    /// index into the `type_ids` list for the return type of this prototype
     pub return_type_idx: u32,
     /// offset from the start of the file to the list of parameter types for this prototype, or 0 if this prototype has no parameters. This offset, if non-zero, should be in the data section, and the data there should be in the format specified by "type_list" below. Additionally, there should be no reference to the type void in the list.
     pub parameters_off: u32,
--- dex2smali/src/main.rs
+++ dex2smali/src/main.rs
@@ -16,10 +16,15 @@ fn print_field(dex: &Dex, field: &EncodedField) {
 fn print_method(dex: &Dex, method: &dex::encoded::EncodedMethod) {
     let method_id = &dex.method_ids[method.method_idx as usize];
     let method_class = &dex.types[method_id.class_idx as usize];
-    let method_type = &dex.types[method_id.proto_idx as usize];
+    let proto_id = &dex.proto_ids[method_id.proto_idx as usize];
+
+    let shorty = &dex.strings[proto_id.shorty_idx as usize];
+    let return_type = &dex.types[proto_id.return_type_idx as usize];
+    let method_proto = format!("{} {}", return_type, shorty);
+
     let method_name = &dex.strings[method_id.name_idx as usize];
 
-    println!("      Method: {} {} {}", method_class, method_type, method_name);
+    println!("      Method: {} {} {}", method_class, method_proto, method_name);
 }
 
 fn main() {
